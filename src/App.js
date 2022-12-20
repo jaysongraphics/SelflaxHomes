1939f6e8fd868d76cd40c4249fedfe5fdc61c358
jaysongraphics <jaysongraphics.com>
2022-12-20 16:27:33 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import BookAStay from './Components/BookAStay'
 import Amenities from './Components/Amenities'
 import AroundUs from './Components/AroundUs'
 import logo from './images/Original_logo1.png'
-import { Link, Route, Routes, NavLink } from 'react-router-dom'
+import { Route, Routes, NavLink, Link } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      <img className="logoImage" src={logo} alt="SelflaxHomes-Logo" width="300" height="270" />
+      <a href="/"><img className="logoImage" src={logo} alt="SelflaxHomes-Logo" width="200" height="180" /></a>
       <nav className="navBar">
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" style={{ textDecoration: 'none' }}>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/Bookastay">Book A Stay</NavLink>
+            <NavLink to="/Bookastay" style={{ textDecoration: 'none' }}>Book A Stay</NavLink>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/" index element={<Home />} />
+        <Route exact path="/" index element={<Home />} />
         <Route path="/Bookastay" element={<BookAStay />} />
 
         {/* <Images /> */}
