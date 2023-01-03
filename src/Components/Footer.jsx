49bd0a9643d3d49a1c5dc9d9b4dd928a9d6cd01a
jaysongraphics <jaysongraphics.com>
2023-01-03 16:24:32 -0500

    changes to navbar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import ig from '../images/ig.png'
 
 function Footer (){
   return (
     <footer className="footer">
         <div id="footer1">
+          <img src={ig} alt="ig-Logo" height="40px" /><p>@SelflaxHomes</p>
           <p>© 2023 All Rights Reserved by SelflaxHomes | Designed by JaysonGraphics</p>
         </div>
     </footer>
